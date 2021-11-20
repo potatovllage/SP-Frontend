06e677d7832fd49516d592d1afaed8bd2195f4fd
Yeong joon <bluehome8626@naver.com>
2021-11-20 14:03:09 +0900

    [UPDATE] Header Style
    style + img

--- shoppingmall/src/components/pages/header/style.js
+++ shoppingmall/src/components/pages/header/style.js
@@ -14,18 +14,19 @@ export const HeadDiv = styled.div`
 `;
 
 export const LogoImg = styled.img`
-  width: 75px;
-  height: 75px;
+  width: 90px;
+  height: 90px;
   margin-right: 70px;
   margin-left: 100px;
 `;
 
 export const Select = styled.input`
-  width: 380px;
+  width: 400px;
   height: 22px;
   padding-top: 2px;
   padding-left: 12px;
   margin-right: 650px;
+  font-family: "Noto Sans KR", sans-serif;
 `;
 
 export const Truckimg = styled.img`
@@ -42,5 +43,70 @@ export const Userimg = styled.img`
 export const Cartimg = styled.img`
   width: 35px;
   height: 35px;
-  margin-right: 50px;
+  margin-right: 70px;
+`;
+
+export const HeadLine = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 8px;
+  background-color: #c0c0c0;
+`;
+
+export const MenuDiv = styled.div`
+  width: 100%;
+  height: 40px;
+`;
+
+export const FStart = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const MenuBtn1 = styled.input`
+  margin-left: 70px;
+  margin-right: 10px;
+  margin-top: 10px;
+  border: none;
+  background-color: white;
+  font-family: "Noto Sans KR", sans-serif;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const MenuBtn = styled.input`
+  margin-right: 15px;
+  margin-top: 10px;
+  border: none;
+  background-color: white;
+  font-family: "Noto Sans KR", sans-serif;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const LoginBtn = styled.input`
+  margin-left: 980px;
+  margin-right: 15px;
+  margin-top: 10px;
+  border: none;
+  background-color: white;
+  font-family: "Noto Sans KR", sans-serif;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 350px;
+  margin-top: 10px;
+  background-color: black;
+`;
+
+export const FieldMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
