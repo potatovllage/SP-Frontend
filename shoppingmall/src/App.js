54cecf0064a0ad95cb3a254dce929eaf813ae789
Yeong joon <bluehome8626@naver.com>
2021-11-20 22:35:46 +0900

    [UPDATE] Louter

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/pages/header/Header";
+import Mainpage from "./components/pages/mainpage/Mainpage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route element={<Header></Header>} path="/" />
+        <Route element={<Mainpage></Mainpage>} path="/" />
       </Routes>
     </BrowserRouter>
   );
