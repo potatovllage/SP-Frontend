072d86fd8d828388960246a1d10484ac47c5134d
Yeong joon <bluehome8626@naver.com>
2021-11-20 13:19:01 +0900

    APP

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/pages/header/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Header></Header>} path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
