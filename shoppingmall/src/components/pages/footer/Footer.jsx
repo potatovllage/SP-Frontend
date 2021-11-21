f5f61297bad873bff035647df78e5def7aec6e25
Yeong joon <bluehome8626@naver.com>
2021-11-21 22:04:35 +0900

    [CREATE] Footer

---
+++ shoppingmall/src/components/pages/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as s from "./style";
+
+function Footer() {
+  return (
+    <s.Foot>
+      <h5>Front 전영준 Back 이준서</h5>
+    </s.Foot>
+  );
+}
+export default Footer;
---
+++ shoppingmall/src/components/pages/footer/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Foot = styled.div`
+  width: 100%;
+  height: 200px;
+  margin-top: 50px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
