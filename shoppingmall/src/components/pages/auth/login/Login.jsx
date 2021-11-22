fe7129aa41e072dd7e7099c7cd694ed443a73573
Yeong joon <bluehome8626@naver.com>
2021-11-22 23:04:57 +0900

    [UPDATE] Login

--- shoppingmall/src/components/pages/auth/login/Login.jsx
+++ shoppingmall/src/components/pages/auth/login/Login.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../../../assets/logo.jpg";
+import * as s from "./style";
 
 function Login() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const mainHistory = () => {
+    navigate("/");
+  };
+
+  return (
+    <s.LoginDiv>
+      <s.Loginform>
+        <s.Loginlogo src={logo} alt="logo" onClick={mainHistory} />
+        <s.LoginE type="text" placeholder="E-mail" />
+        <s.LoginId type="text" placeholder="ID" />
+        <s.LoginPw type="text" placeholder="Password" />
+        <s.LoginSub type="button" value="LOGIN" />
+        <Link to="/signUp">
+          <s.LinkSign type="button" value="회원가입" />
+        </Link>
+      </s.Loginform>
+    </s.LoginDiv>
+  );
 }
 export default Login;
--- shoppingmall/src/components/pages/auth/login/style.js
+++ shoppingmall/src/components/pages/auth/login/style.js
@@ -1,1 +1,76 @@
-import styleds from "styled-components";
+import styled from "styled-components";
+
+export const LoginDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Loginform = styled.div`
+  width: 350px;
+  height: 530px;
+  border: 5px solid black;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Loginlogo = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+export const LoginE = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 40px;
+  padding-left: 20px;
+  border: solid 1px gray;
+  border-radius: 15px;
+`;
+
+export const LoginId = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 40px;
+  padding-left: 20px;
+  border: solid 1px gray;
+  border-radius: 15px;
+`;
+
+export const LoginPw = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 40px;
+  padding-left: 20px;
+  border: solid 1px gray;
+  border-radius: 15px;
+`;
+
+export const LoginSub = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 50px;
+  padding-left: 20px;
+  border: none;
+  border-radius: 15px;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LinkSign = styled.input`
+  margin-top: 30px;
+  border: none;
+  background-color: white;
+  font-size: 16px;
+  font-family: "Noto Sans KR", sans-serif;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
