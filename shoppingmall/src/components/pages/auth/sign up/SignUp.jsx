3f733bef8d1335ee0592393435b9aeb0f2e57fe2
Yeong joon <bluehome8626@naver.com>
2021-11-23 23:05:25 +0900

    [UPDATE] SignUp

--- shoppingmall/src/components/pages/auth/sign up/SignUp.jsx
+++ shoppingmall/src/components/pages/auth/sign up/SignUp.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-/*import * as s from "./style";*/
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../../../assets/logo.jpg";
+import * as s from "./style";
 
 function SignUp() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const mainHistory = () => {
+    navigate("/");
+  };
+  return (
+    <s.SignDiv>
+      <s.Signform>
+        <s.Signlogo src={logo} alt="logo" onClick={mainHistory} />
+        <s.SignE type="text" placeholder="E-mail" />
+        <s.SignId type="text" placeholder="ID" />
+        <s.SignPw type="text" placeholder="Password" />
+        <s.SignPw type="text" placeholder="NickName" />
+        <s.SignSub type="button" value="SIGN UP" />
+        <Link to="/login">
+          <s.Linklogin type="button" value="로그인" />
+        </Link>
+      </s.Signform>
+    </s.SignDiv>
+  );
 }
 export default SignUp;
--- shoppingmall/src/components/pages/auth/sign up/style.js
+++ shoppingmall/src/components/pages/auth/sign up/style.js
@@ -1,5 +1,77 @@
 import styled from "styled-components";
 
-export const signDiv = styled.div`
+export const SignDiv = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Signform = styled.div`
+  width: 350px;
+  height: 530px;
+  border: 5px solid black;
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Signlogo = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+export const SignE = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 40px;
+  padding-left: 20px;
+  border: solid 1px gray;
+  border-radius: 15px;
+`;
+
+export const SignId = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 40px;
+  padding-left: 20px;
+  border: solid 1px gray;
+  border-radius: 15px;
+`;
+
+export const SignPw = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 40px;
+  padding-left: 20px;
+  border: solid 1px gray;
+  border-radius: 15px;
+`;
+
+export const SignSub = styled.input`
+  width: 250px;
+  height: 30px;
+  margin-top: 50px;
+  padding-left: 20px;
+  border: none;
+  border-radius: 15px;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Linklogin = styled.input`
+  margin-top: 30px;
+  margin-bottom: 20px;
+  border: none;
+  background-color: white;
+  font-size: 16px;
+  font-family: "Noto Sans KR", sans-serif;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
