c6c534b257a3467ee5a468552aaa2b1cef879ad1
Yeong joon <bluehome8626@naver.com>
2021-11-26 11:00:18 +0900

    [CREATE] Coupon style

---
+++ shoppingmall/src/components/pages/coupon/style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Interver = styled.div`
+  width: 100%;
+  height: 120px;
+  margin-top: 15px;
+  background-color: #dcdcdc;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Coupon = styled.div`
+  width: 100%;
+  height: 1500px;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Couponform = styled.div`
+  width: 1000px;
+  height: 1400px;
+  background-color: white;
+`;
+
+export const CouponBtn = styled.input`
+  width: 500px;
+  height: 55px;
+  margin-top: 65px;
+  border: none;
+  border-radius: 9px 9px 0 0;
+  box-shadow: 0px 1px 10px 5px gray;
+  font-size: 18px;
+`;
+
+export const BenefitsBtn = styled.input`
+  width: 500px;
+  height: 55px;
+  margin-top: 65px;
+  border: none;
+  border-radius: 9px 9px 0 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  font-size: 18px;
+  color: white;
+`;
+
+export const CouponBanner = styled.img`
+  width: 1000px;
+  height: 120px;
+`;
