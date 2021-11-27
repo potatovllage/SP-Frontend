575a6d07bd002d6beeac07fb27fee82a4430a9ae
Yeong joon <bluehome8626@naver.com>
2021-11-27 23:41:11 +0900

    [UPDATE] Coupon

--- shoppingmall/src/components/pages/coupon/Coupon.jsx
+++ shoppingmall/src/components/pages/coupon/Coupon.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "../header/Header";
 import banner4 from "../../../assets/banner4.png";
+import coupon1 from "../../../assets/coupon1.png";
+import coupon2 from "../../../assets/coupon2.png";
+import coupon3 from "../../../assets/coupon3.png";
+import coupon4 from "../../../assets/coupon4.png";
+import coupon5 from "../../../assets/coupon5.png";
 import * as s from "./style";
 
 function Coupon() {
@@ -14,6 +19,11 @@ function Coupon() {
       <s.Coupon>
         <s.Couponform>
           <s.CouponBanner src={banner4} alt="logo" />
+          <s.Coupon1 src={coupon1} alt="logo" />
+          <s.Coupon2 src={coupon2} alt="logo" />
+          <s.Coupon3 src={coupon3} alt="logo" />
+          <s.Coupon4 src={coupon4} alt="logo" />
+          <s.Coupon5 src={coupon5} alt="logo" />
         </s.Couponform>
       </s.Coupon>
     </>
