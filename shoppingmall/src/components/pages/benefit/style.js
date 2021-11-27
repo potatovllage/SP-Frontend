b185f2af64676303de90600318165a9c79ae8248
Yeong joon <bluehome8626@naver.com>
2021-11-28 00:18:59 +0900

    [UPDATE] Benefit Style

---
+++ shoppingmall/src/components/pages/benefit/style.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+export const Interver = styled.div`
+  width: 100%;
+  height: 120px;
+  margin-top: 15px;
+  background-color: #dcdcdc;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Benefit = styled.div`
+  width: 100%;
+  height: 1400px;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Benefitform = styled.div`
+  width: 1000px;
+  height: 1300px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CouponBtn = styled.input`
+  width: 500px;
+  height: 55px;
+  margin-top: 65px;
+  border: none;
+  border-radius: 9px 9px 0 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  font-size: 18px;
+  color: white;
+`;
+
+export const BenefitsBtn = styled.input`
+  width: 500px;
+  height: 55px;
+  margin-top: 65px;
+  border: none;
+  border-radius: 9px 9px 0 0;
+  box-shadow: 0px 1px 10px 5px gray;
+  font-size: 18px;
+`;
+
+export const BenefitBanner = styled.img`
+  width: 1000px;
+  height: 120px;
+`;
+
+export const Benefit1 = styled.img`
+  width: 500px;
+  height: 200px;
+  margin-top: 20px;
+  background-color: black;
+`;
+
+export const Benefit2 = styled.img`
+  width: 500px;
+  height: 200px;
+  margin-top: 20px;
+  background-color: black;
+`;
+
+export const Benefit3 = styled.img`
+  width: 500px;
+  height: 200px;
+  margin-top: 20px;
+  background-color: black;
+`;
+
+export const Benefit4 = styled.img`
+  width: 500px;
+  height: 200px;
+  margin-top: 20px;
+  background-color: black;
+`;
+
+export const Benefit5 = styled.img`
+  width: 500px;
+  height: 200px;
+  margin-top: 20px;
+  margin-bottom: 80px;
+  background-color: black;
+`;
