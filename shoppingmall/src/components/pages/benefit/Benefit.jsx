62b0615ebe98ddd4d4983a0fc3439b8b53035394
Yeong joon <bluehome8626@naver.com>
2021-11-28 00:19:13 +0900

    [CREATE] Benefit

---
+++ shoppingmall/src/components/pages/benefit/Benefit.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../header/Header";
+import banner5 from "../../../assets/banner5.png";
+import benefit1 from "../../../assets/benefit1.png";
+import benefit2 from "../../../assets/benefit2.png";
+import benefit3 from "../../../assets/benefit3.png";
+import benefit4 from "../../../assets/benefit4.png";
+import benefit5 from "../../../assets/benefit5.png";
+import * as s from "./style";
+
+function Benefit() {
+  return (
+    <>
+      <Header />
+      <s.Interver>
+        <Link to="/coupon">
+          <s.CouponBtn type="button" value="할인은 이곳 쿠폰존" />
+        </Link>
+        <Link to="/benefit">
+          <s.BenefitsBtn type="button" value="모든 혜택은 이곳 혜택존" />
+        </Link>
+      </s.Interver>
+      <s.Benefit>
+        <s.Benefitform>
+          <s.BenefitBanner src={banner5} alt="logo" />
+          <s.Benefit1 src={benefit1} alt="logo" />
+          <s.Benefit2 src={benefit2} alt="logo" />
+          <s.Benefit3 src={benefit3} alt="logo" />
+          <s.Benefit4 src={benefit4} alt="logo" />
+          <s.Benefit5 src={benefit5} alt="logo" />
+        </s.Benefitform>
+      </s.Benefit>
+    </>
+  );
+}
+export default Benefit;
