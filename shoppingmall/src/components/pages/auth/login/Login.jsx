6a733d5e2af78f621af65d91b3b1d5f2b10739e3
Yeong joon <bluehome8626@naver.com>
2021-11-28 18:04:10 +0900

    [UPDATE] Login Success

--- shoppingmall/src/components/pages/auth/login/Login.jsx
+++ shoppingmall/src/components/pages/auth/login/Login.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../../../assets/logo.jpg";
 import * as s from "./style";
+import axios from "axios";
 
 function Login() {
   const navigate = useNavigate();
@@ -10,14 +11,84 @@ function Login() {
     navigate("/");
   };
 
+  const JWT_EXPIRY_TIME = 24 * 3600 * 1000;
+
+  const [input, setInput] = useState({
+    email: "",
+    userid: "",
+    password: "",
+  });
+
+  const { email, userid, password } = input;
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onLogin = (e) => {
+    axios
+      .post("http://13.125.241.207:8088/auth/login", {
+        email: email,
+        userid: userid,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data.accessToken);
+        localStorage.setItem("accessToken", res.data.accessToken);
+        navigate("/");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    const onReissue = () => {
+      axios
+        .post("http://13.125.241.207:8088/auth/reissue")
+        .then(onLoginSuccess)
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+
+    const onLoginSuccess = (Response) => {
+      const { accessToken } = Response.data;
+
+      // accessToken 설정
+      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+      // accessToken 만료하기 1분 전에 로그인 연장
+      setTimeout(onReissue, JWT_EXPIRY_TIME - 6000);
+    };
+  };
+
   return (
     <s.LoginDiv>
       <s.Loginform>
         <s.Loginlogo src={logo} alt="logo" onClick={mainHistory} />
-        <s.LoginE type="text" placeholder="E-mail" />
-        <s.LoginId type="text" placeholder="ID" />
-        <s.LoginPw type="text" placeholder="Password" />
-        <s.LoginSub type="button" value="LOGIN" />
+        <s.LoginE
+          name="email"
+          type="text"
+          placeholder="E-mail"
+          onChange={onChange}
+        />
+        <s.LoginId
+          name="userid"
+          type="text"
+          placeholder="ID"
+          onChange={onChange}
+        />
+        <s.LoginPw
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={onChange}
+        />
+        <s.LoginSub type="button" value="LOGIN" onClick={onLogin} />
         <Link to="/signUp">
           <s.LinkSign type="button" value="회원가입" />
         </Link>
