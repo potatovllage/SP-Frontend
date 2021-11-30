7155b58db1abce318941857731e4c086832dcf0b
Yeong joon <bluehome8626@naver.com>
2021-11-30 16:57:50 +0900

    [UPDATE] Ticket + Style

--- shoppingmall/src/components/pages/morepage/ticket/Ticket.jsx
+++ shoppingmall/src/components/pages/morepage/ticket/Ticket.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import Header from "../../header/Header";
+import * as s from "./style";
 
 function Ticket() {
+  const menus = [
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+  ];
+
+  const TicketList = menus.map((menu, index) => {
+    return (
+      <>
+        <s.TicketProduct>{menu}</s.TicketProduct>
+      </>
+    );
+  });
   return (
     <>
-      <Header></Header>
+      <Header />
+      <s.Interver>
+        <h2>BOOK</h2>
+      </s.Interver>
+      <s.Ticket>
+        <s.TicketList>{TicketList}</s.TicketList>
+      </s.Ticket>
     </>
   );
 }
