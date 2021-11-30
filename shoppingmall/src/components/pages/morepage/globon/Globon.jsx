5fe08cb602c57059410fdb95f5b0fe8b9691cae2
Yeong joon <bluehome8626@naver.com>
2021-11-30 16:57:12 +0900

    [UPDATE] Globon + Style

--- shoppingmall/src/components/pages/morepage/globon/Globon.jsx
+++ shoppingmall/src/components/pages/morepage/globon/Globon.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import Header from "../../header/Header";
+import * as s from "./style";
 
 function Globon() {
+  const menus = [
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+  ];
+
+  const GlobonList = menus.map((menu, index) => {
+    return (
+      <>
+        <s.GlobonProduct>{menu}</s.GlobonProduct>
+      </>
+    );
+  });
   return (
     <>
-      <Header></Header>
+      <Header />
+      <s.Interver>
+        <h2>해외직구</h2>
+      </s.Interver>
+      <s.Globon>
+        <s.GlobonList>{GlobonList}</s.GlobonList>
+      </s.Globon>
     </>
   );
 }
