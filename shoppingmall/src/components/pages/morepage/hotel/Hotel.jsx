c1e1ca4cda3033e85bf060bf8fc2f7724eb7039c
Yeong joon <bluehome8626@naver.com>
2021-11-30 16:57:33 +0900

    [UPDATE] Hotel + Style

--- shoppingmall/src/components/pages/morepage/hotel/Hotel.jsx
+++ shoppingmall/src/components/pages/morepage/hotel/Hotel.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import Header from "../../header/Header";
+import * as s from "./style";
 
 function Hotel() {
+  const menus = [
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+  ];
+
+  const HotelList = menus.map((menu, index) => {
+    return (
+      <>
+        <s.HotelProduct>{menu}</s.HotelProduct>
+      </>
+    );
+  });
   return (
     <>
-      <Header></Header>
+      <Header />
+      <s.Interver>
+        <h2>BOOK</h2>
+      </s.Interver>
+      <s.Hotel>
+        <s.HotelList>{HotelList}</s.HotelList>
+      </s.Hotel>
     </>
   );
 }
