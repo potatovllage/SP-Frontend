81c30116c43b3869d2d465a35b5a7e3d717f74b4
Yeong joon <bluehome8626@naver.com>
2021-11-30 16:57:01 +0900

    [UPDATE] Car + Style

--- shoppingmall/src/components/pages/morepage/car/Car.jsx
+++ shoppingmall/src/components/pages/morepage/car/Car.jsx
@@ -3,10 +3,41 @@ import Header from "../../header/Header";
 import * as s from "./style";
 
 function Car() {
+  const menus = [
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+  ];
+
+  const CarList = menus.map((menu, index) => {
+    return (
+      <>
+        <s.CarProduct>{menu}</s.CarProduct>
+      </>
+    );
+  });
   return (
     <>
       <Header />
-      <s.Car></s.Car>
+      <s.Interver>
+        <h2>자동차용품</h2>
+      </s.Interver>
+      <s.Car>
+        <s.CarList>{CarList}</s.CarList>
+      </s.Car>
     </>
   );
 }
--- shoppingmall/src/components/pages/morepage/car/style.js
+++ shoppingmall/src/components/pages/morepage/car/style.js
@@ -2,4 +2,36 @@ import styled from "styled-components";
 
 export const Car = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CarList = styled.div`
+  width: 1200px;
+  height: 1400px;
+  background-color: #dcdcdc;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+export const Interver = styled.div`
+  width: 100%;
+  height: 120px;
+  margin-top: 15px;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CarProduct = styled.div`
+  width: 250px;
+  height: 300px;
+  margin: 20px 0px 0px 20px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
