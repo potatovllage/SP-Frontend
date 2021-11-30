09d9d596fb0fea8fd6410f0e07396661db2216e3
Yeong joon <bluehome8626@naver.com>
2021-11-30 16:44:33 +0900

    [UPDATE] Book + Style

--- shoppingmall/src/components/pages/morepage/book/Book.jsx
+++ shoppingmall/src/components/pages/morepage/book/Book.jsx
@@ -3,11 +3,41 @@ import Header from "../../header/Header";
 import * as s from "./style";
 
 function Book() {
+  const menus = [
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+    "메인이미지 상품명",
+  ];
+
+  const BookList = menus.map((menu, index) => {
+    return (
+      <>
+        <s.BookProduct>{menu}</s.BookProduct>
+      </>
+    );
+  });
   return (
     <>
-      <Header>
-        <s.Book></s.Book>
-      </Header>
+      <Header />
+      <s.Interver>
+        <h2>BOOK</h2>
+      </s.Interver>
+      <s.Book>
+        <s.BookList>{BookList}</s.BookList>
+      </s.Book>
     </>
   );
 }
--- shoppingmall/src/components/pages/morepage/book/style.js
+++ shoppingmall/src/components/pages/morepage/book/style.js
@@ -2,4 +2,36 @@ import styled from "styled-components";
 
 export const Book = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BookList = styled.div`
+  width: 1200px;
+  height: 1400px;
+  background-color: #dcdcdc;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+export const Interver = styled.div`
+  width: 100%;
+  height: 120px;
+  margin-top: 15px;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BookProduct = styled.div`
+  width: 250px;
+  height: 300px;
+  margin: 20px 0px 0px 20px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
