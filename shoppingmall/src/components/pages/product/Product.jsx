0603f6c63603babed2ff27b518c64ab5a4f8d11c
Yeong joon <bluehome8626@naver.com>
2021-12-01 10:20:44 +0900

    [UPDATE] Product + Style

--- shoppingmall/src/components/pages/product/Product.jsx
+++ shoppingmall/src/components/pages/product/Product.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Header from "../header/Header";
+import * as s from "./style";
 
 function Product() {
+  const [Quantity, setQuantity] = useState(0);
+  const detailRef = useRef();
+
+  const productInfo = {
+    name: "제품명",
+    price: "15000원",
+  };
+
+  const onIncrease = () => {
+    const pCount = Quantity + 1;
+    setQuantity(pCount);
+  };
+
+  const onDecrease = () => {
+    const mCount = Quantity - 1;
+    if (mCount < 0) {
+      alert("0 이하로 내려갈 수 없습니다.");
+    } else {
+      setQuantity(mCount);
+    }
+  };
+
+  const PDetail = [
+    "쿠폰을 선택해주세요",
+    "1. 기획 이벤트! 15% 할인",
+    "2. 쇼핑몰 출시 이벤트! 3000원 할인",
+    "3. 2000원 할인",
+    "4. 반값? 50% 할인",
+    "5. 이벤트! 5000원 할인",
+  ];
   return (
     <>
       <Header />
+      <s.Product>
+        <s.ProductBoard>
+          <s.ProductIMG />
+          <s.ProductName>
+            <h2>{`${productInfo.name}`}</h2>
+          </s.ProductName>
+          <s.Line />
+          <s.ProductPrice>
+            <h2>{`${productInfo.price}`}</h2>
+          </s.ProductPrice>
+        </s.ProductBoard>
+      </s.Product>
     </>
   );
 }
---
+++ shoppingmall/src/components/pages/product/style.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const Product = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProductBoard = styled.div`
+  width: 1000px;
+  height: 1200px;
+  margin-top: 80px;
+  border: 5px solid black;
+  background-color: #f8f8ff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProductIMG = styled.div`
+  width: 400px;
+  height: 430px;
+  border: 5px solid black;
+  margin-left: 20px;
+  margin-bottom: 650px;
+`;
+
+export const ProductName = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProductPrice = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Line = styled.div`
+  width: 300px;
+  height: 1px;
+  background-color: black;
+`;
+
+export const H2 = styled.h2`
+  margin-top: 0;
+  margin-right: 30px;
+`;
+
+export const ProductQuantity = styled.div`
+  width: 180px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border: 3px solid black;
+  margin-bottom: 10px;
+`;
+
+export const QUp = styled.input`
+  width: 30px;
+  height: 20px;
+  margin-right: 10px;
+  margin-left: 10px;
+`;
+
+export const QDown = styled.input`
+  width: 30px;
+  height: 20px;
+  margin-right: 10px;
+  margin-left: 10px;
+`;
+
+export const ProductBuy = styled.div`
+  margin-top: 100px;
+  margin-bottom: 300px;
+`;
+
+export const BasketBtn = styled.input`
+  width: 150px;
+  height: 30px;
+  background-color: white;
+  color: black;
+  margin-right: 10px;
+  border: 1px solid black;
+`;
+
+export const BuyBtn = styled.input`
+  width: 150px;
+  height: 30px;
+  background-color: black;
+  color: white;
+  border: none;
+`;
