bddf8d6b6bd752fbbbeafeeee968590e5375d396
Yeong joon <bluehome8626@naver.com>
2021-12-01 10:20:10 +0900

    [CREATE] Basket + Style

---
+++ shoppingmall/src/components/pages/basket/Basket.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../header/Header";
+import * as s from "./style";
+
+function Basket() {
+  return (
+    <>
+      <Header />
+      <s.Basket>
+        <s.Basketform>
+          <s.basketname>장바구니</s.basketname>
+        </s.Basketform>
+      </s.Basket>
+    </>
+  );
+}
+export default Basket;
---
+++ shoppingmall/src/components/pages/basket/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Basket = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const basketname = styled.h2`
+  margin-left: 20px;
+  margin-bottom: 20px;
+`;
+
+export const Basketform = styled.div`
+  width: 1200px;
+  height: 800px;
+  border: 5px solid black;
+  margin-top: 50px;
+`;
