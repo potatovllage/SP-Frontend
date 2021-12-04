b5133ca3bbf13f76360baeb2e8d2ba7ca0f7243a
Yeong joon <bluehome8626@naver.com>
2021-12-04 21:13:49 +0900

    [UPDATE] AddProduct + Style

--- shoppingmall/src/components/pages/addproduct/AddProduct.jsx
+++ shoppingmall/src/components/pages/addproduct/AddProduct.jsx
@@ -6,7 +6,20 @@ function AddProduct() {
   return (
     <>
       <Header />
-      <s.AddProduct></s.AddProduct>
+      <s.AddProduct>
+        <s.AddForm>
+          <s.ImgForm>
+            <s.H2>이미지 추가</s.H2>
+            <s.AddImg>+</s.AddImg>
+          </s.ImgForm>
+          <s.InfoAdd>
+            <s.H2>상품명</s.H2>
+            <s.AddName type="text" />
+            <s.H2>상품 가격</s.H2>
+            <s.AddFrice type="number" />
+          </s.InfoAdd>
+        </s.AddForm>
+      </s.AddProduct>
     </>
   );
 }
--- shoppingmall/src/components/pages/addproduct/style.js
+++ shoppingmall/src/components/pages/addproduct/style.js
@@ -4,3 +4,42 @@ export const AddProduct = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const H2 = styled.h2``;
+
+export const AddForm = styled.div`
+  width: 1200px;
+  height: 800px;
+  padding: 25px;
+  border: 5px solid black;
+  margin-top: 50px;
+`;
+
+export const ImgForm = styled.div``;
+
+export const AddImg = styled.div`
+  width: 450px;
+  height: 450px;
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 40px;
+`;
+
+export const InfoAdd = styled.div``;
+
+export const AddName = styled.input`
+  width: 300px;
+  height: 25px;
+  padding-left: 3px;
+`;
+
+export const AddFrice = styled.input`
+  width: 80px;
+  height: 25px;
+  padding-left: 3px;
+  font-size: 20px;
+  text-align: center;
+`;
