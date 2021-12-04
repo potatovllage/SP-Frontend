3b9bfbeffaafd92c7967760e25faed49e1a9c7a2
Yeong joon <bluehome8626@naver.com>
2021-12-04 16:52:32 +0900

    [CREATE] AddProduct + Style

---
+++ shoppingmall/src/components/pages/addproduct/AddProduct.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as s from "./style";
+import Header from "../header/Header";
+
+function AddProduct() {
+  return (
+    <>
+      <Header />
+      <s.AddProduct></s.AddProduct>
+    </>
+  );
+}
+export default AddProduct;
