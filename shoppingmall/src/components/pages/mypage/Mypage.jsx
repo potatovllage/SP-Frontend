02d17bf64038f49b2ee9d03025901b007504dc5d
Yeong joon <bluehome8626@naver.com>
2021-12-04 23:19:34 +0900

    [UPDATE] 삼항연산자

--- shoppingmall/src/components/pages/mypage/Mypage.jsx
+++ shoppingmall/src/components/pages/mypage/Mypage.jsx
@@ -9,19 +9,38 @@ function Mypage() {
   };
   return (
     <>
-      <Header />
-      <s.Mypage>
-        <s.MypageMenu>
-          <s.MyName>{MyInfo.name}</s.MyName>
-          <Link to="/addProduct">
-            <s.AddProduct type="button" value="상품 등록" />
-          </Link>
-        </s.MypageMenu>
-        <s.GoodName>좋아요한 상품</s.GoodName>
-        <s.Goodform></s.Goodform>
-        <s.BuyName>구매 내역</s.BuyName>
-        <s.Goodform></s.Goodform>
-      </s.Mypage>
+      {localStorage.getItem("accessToken") ? (
+        <>
+          <Header />
+          <s.Mypage>
+            <s.MypageMenu>
+              <s.MyName>{MyInfo.name}</s.MyName>
+              <Link to="/addProduct">
+                <s.AddProduct type="button" value="상품 등록" />
+              </Link>
+            </s.MypageMenu>
+            <s.GoodName>좋아요한 상품</s.GoodName>
+            <s.Goodform></s.Goodform>
+            <s.BuyName>구매 내역</s.BuyName>
+            <s.Goodform></s.Goodform>
+          </s.Mypage>
+        </>
+      ) : (
+        <>
+          <Header />
+          <s.AuthHead>
+            <s.H2>로그인 및 회원가입을 해주세요</s.H2>
+          </s.AuthHead>
+          <s.MypageAuth>
+            <Link to="/login">
+              <s.LoginBtn type="button" value="로그인" />
+            </Link>
+            <Link to="/signUp">
+              <s.SignBtn type="button" value="회원가입" />
+            </Link>
+          </s.MypageAuth>
+        </>
+      )}
     </>
   );
 }
--- shoppingmall/src/components/pages/mypage/style.js
+++ shoppingmall/src/components/pages/mypage/style.js
@@ -56,3 +56,36 @@ export const Buyform = styled.div`
   margin-top: 10px;
   display: flex;
 `;
+
+export const MypageAuth = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginBtn = styled.input`
+  width: 150px;
+  height: 50px;
+  margin-right: 50px;
+  border: 1px solid black;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+`;
+
+export const SignBtn = styled.input`
+  width: 150px;
+  height: 50px;
+  border: 1px solid black;
+  background-color: white;
+  cursor: pointer;
+`;
+
+export const AuthHead = styled.div`
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const H2 = styled.h2``;
