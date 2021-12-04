ed3070fe70ba09f4e6b9aa71588be230b01128ab
Yeong joon <bluehome8626@naver.com>
2021-12-04 21:51:26 +0900

    [UPDATE] AddProduct + Style

--- shoppingmall/src/components/pages/addproduct/AddProduct.jsx
+++ shoppingmall/src/components/pages/addproduct/AddProduct.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import * as s from "./style";
 import Header from "../header/Header";
 
 function AddProduct() {
+  const [Quantity, setQuantity] = useState(0);
+  const detailRef = useRef();
+
+  const onIncrease = () => {
+    const pCount = Quantity + 1;
+    setQuantity(pCount);
+  };
+
+  const onDecrease = () => {
+    const mCount = Quantity - 1;
+    if (mCount < 0) {
+      alert("0 이하로 내려갈 수 없습니다.");
+    } else {
+      setQuantity(mCount);
+    }
+  };
+
+  const Category = [
+    "카테고리를 설정해주세요",
+    "1. 베스트딜",
+    "2. 할인상품",
+    "3. 쿠폰/혜택",
+    "4. 당일배송",
+    "5. 이벤트! 5000원 할인",
+    "6. 책",
+    "7. 자동차용품",
+    "8. 호텔",
+    "9. 해외직구",
+    "10. 티켓",
+  ];
+
   return (
     <>
       <Header />
@@ -16,7 +47,19 @@ function AddProduct() {
             <s.H2>상품명</s.H2>
             <s.AddName type="text" />
             <s.H2>상품 가격</s.H2>
-            <s.AddFrice type="number" />
+            <s.AddFrice type="text" />
+            <s.H2>수량</s.H2>
+            <s.ProductQuantity>
+              <s.QDown type="button" value="-" onClick={onDecrease} />
+              <s.H2>{Quantity}</s.H2>
+              <s.QUp type="button" value="+" onClick={onIncrease} />
+            </s.ProductQuantity>
+            <s.H2>카테고리</s.H2>
+            <s.Select id="selectList" ref={detailRef}>
+              {Category.map((detail) => (
+                <option key={detail}>{detail}</option>
+              ))}
+            </s.Select>
           </s.InfoAdd>
         </s.AddForm>
       </s.AddProduct>
--- shoppingmall/src/components/pages/addproduct/style.js
+++ shoppingmall/src/components/pages/addproduct/style.js
@@ -10,12 +10,16 @@ export const H2 = styled.h2``;
 export const AddForm = styled.div`
   width: 1200px;
   height: 800px;
-  padding: 25px;
+  padding: 20px;
   border: 5px solid black;
   margin-top: 50px;
+  display: flex;
+  justify-content: center;
 `;
 
-export const ImgForm = styled.div``;
+export const ImgForm = styled.div`
+  margin-right: 200px;
+`;
 
 export const AddImg = styled.div`
   width: 450px;
@@ -28,7 +32,9 @@ export const AddImg = styled.div`
   font-size: 40px;
 `;
 
-export const InfoAdd = styled.div``;
+export const InfoAdd = styled.div`
+  margin-top: 30px;
+`;
 
 export const AddName = styled.input`
   width: 300px;
@@ -37,9 +43,37 @@ export const AddName = styled.input`
 `;
 
 export const AddFrice = styled.input`
-  width: 80px;
+  width: 300px;
   height: 25px;
   padding-left: 3px;
-  font-size: 20px;
-  text-align: center;
+`;
+
+export const ProductQuantity = styled.div`
+  width: 180px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid black;
+  margin-bottom: 10px;
+`;
+
+export const QUp = styled.input`
+  width: 30px;
+  height: 20px;
+  margin-right: 10px;
+  margin-left: 10px;
+  cursor: pointer;
+`;
+
+export const QDown = styled.input`
+  width: 30px;
+  height: 20px;
+  margin-right: 10px;
+  margin-left: 10px;
+  cursor: pointer;
+`;
+
+export const Select = styled.select`
+  cursor: pointer;
 `;
