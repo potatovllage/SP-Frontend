15bfb9e93096bc5be1df85aecb073d7c18c67144
Yeong joon <bluehome8626@naver.com>
2021-12-06 22:39:02 +0900

    [UPDATE] params 사용 및 쓸모없는 import 삭제

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -5,17 +5,11 @@ import SignUp from "./components/pages/auth/sign up/SignUp";
 import TruckPage from "./components/pages/truckpage/TruckPage";
 import Coupon from "./components/pages/coupon/Coupon";
 import Benefit from "./components/pages/benefit/Benefit";
-import Book from "./components/pages/morepage/book/Book";
-import Car from "./components/pages/morepage/car/Car";
-import Globon from "./components/pages/morepage/globon/Globon";
-import Hotel from "./components/pages/morepage/hotel/Hotel";
-import Ticket from "./components/pages/morepage/ticket/Ticket";
-import Today from "./components/pages/morepage/today/Today";
-import Best from "./components/pages/morepage/best/Best";
-import Discount from "./components/pages/morepage/discount/Discount";
+import List from "./components/pages/list/List";
 import Product from "./components/pages/product/Product";
 import Basket from "./components/pages/basket/Basket";
 import Mypage from "./components/pages/mypage/Mypage";
+import AddProduct from "./components/pages/addproduct/AddProduct";
 import Footer from "./components/pages/footer/Footer";
 
 function App() {
@@ -27,18 +21,12 @@ function App() {
         <Route element={<SignUp />} path="/signUp" />
         <Route element={<Coupon />} path="/coupon" />
         <Route element={<Benefit />} path="/benefit" />
-        <Route element={<Book />} path="/book" />
-        <Route element={<Car />} path="/car" />
-        <Route element={<Globon />} path="/globon" />
-        <Route element={<Hotel />} path="/hotel" />
-        <Route element={<Ticket />} path="/ticket" />
-        <Route element={<Today />} path="/today" />
-        <Route element={<Best />} path="/best" />
-        <Route element={<Discount />} path="/discount" />
+        <Route element={<List />} path="/list/:listname" />
         <Route element={<TruckPage />} path="/truck" />
         <Route element={<Product />} path="/product" />
         <Route element={<Basket />} path="/basket" />
         <Route element={<Mypage />} path="/mypage" />
+        <Route element={<AddProduct />} path="/addProduct" />
       </Routes>
       <Footer />
     </BrowserRouter>
